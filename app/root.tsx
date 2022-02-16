f36aec36b666959ab9fcfb3c0976a7b879b35abe
heinerbehrends <heinerbehrends@hotmail.com>
2022-02-16 11:44:52 +0100

    Added global styles, preconnect links

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Links,
   LiveReload,
@@ -6,11 +7,24 @@ import {
   Scripts,
   ScrollRestoration,
 } from 'remix';
-import type { MetaFunction } from 'remix';
+import type { MetaFunction, LinksFunction } from 'remix';
+import styles from '~/styles/global.css';
 
 export const meta: MetaFunction = () => {
   return { title: 'Giphy Concentration' };
 };
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: styles },
+  { rel: 'preconnect', href: 'https://media.giphy.com' },
+  { rel: 'preconnect', href: 'https://api.giphy.com' },
+  { rel: 'preconnect', href: 'https://fonts.googleapsis.com' },
+  { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+  {
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Inter&display=swap',
+  },
+];
 
 export default function App() {
   return (
