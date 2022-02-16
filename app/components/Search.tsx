1e3e25b145ecc86b089abecde452580cf997f5cc
heinerbehrends <heinerbehrends@hotmail.com>
2022-02-16 12:22:01 +0100

    Removed play again button

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,5 +1,4 @@
 import React, { ChangeEventHandler, FormEventHandler } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Form } from 'remix';
 
 type SearchProps = {
@@ -13,19 +12,11 @@ export default function Search({
   handleSubmit,
   handleChange,
   value,
-  nrOfCardsTurned,
 }: SearchProps) {
-  const navigate = useNavigate();
   return (
     <section className="search-section">
-      {nrOfCardsTurned > 0 ? (
-        <button className="replay-button" onClick={() => navigate(-1)}>
-          Play again!
-        </button>
-      ) : null}
-
-      <h1 className="heading">Search Giphy to start a game</h1>
-      <form className="Form" onSubmit={handleSubmit}>
+      <h1>Search Giphy to start a game</h1>
+      <Form className="Form" onSubmit={handleSubmit}>
         <input
           className="input"
           type="text"
@@ -37,7 +28,7 @@ export default function Search({
         <button className="input button" type="submit">
           Go!
         </button>
-      </form>
+      </Form>
     </section>
   );
 }
