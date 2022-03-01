28f8b69f1090291041b93ba5a73b14bb5a5607f1
heinerbehrends <heinerbehrends@hotmail.com>
2022-03-01 10:47:24 +0100

    Removed nrOfCardsTurned from index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Search from '../components/Search';
 import { useSearch } from '../logic/useSearch';
 import styles from '~/styles/index.css';
@@ -7,18 +6,13 @@ export function links() {
   return [{ rel: 'stylesheet', href: styles }];
 }
 
-type HomeProps = {
-  nrOfCardsTurned: number;
-};
-
-export default function Home({ nrOfCardsTurned }: HomeProps) {
+export default function Home() {
   const { input, handleChange, handleSubmit } = useSearch();
   return (
     <Search
       handleChange={handleChange}
       handleSubmit={handleSubmit}
       value={input}
-      nrOfCardsTurned={nrOfCardsTurned}
     />
   );
 }
